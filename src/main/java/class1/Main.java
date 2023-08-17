c2aa45b184589c70cd29b96291c347e5e531cffc
Mostafa <mostfa.g.m@gmail.com>
2023-08-17 19:19:50 +0300

    start working with Selenium :D with small exe

---
+++ src/main/java/class1/Main.java
@@ -0,0 +1,53 @@
+package class1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utils.Drivers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        wikiSearch("Aardvark", "bear");
+        wikiSearch("Lion", "king");
+    }
+
+    public static void wikiSearch(String searchInput, String countInput) {
+        System.setProperty("webdriver.chrome.driver", Drivers.getChromeDriver());
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.wikipedia.org");
+
+        //find the searchBar and add the input to search
+        WebElement searchBar = driver.findElement(By.id("searchInput"));
+        searchBar.sendKeys(searchInput);
+
+        //find the search button and click it
+        WebElement button = driver.findElement(By.xpath("//form[@id='search-form']//button[@type='submit']"));
+        button.click();
+
+        //get all the body content and save it in a string
+        String pageText = driver.findElement(By.tagName("body")).getText();
+        driver.close();
+
+        //count and print how many the word appeared
+        int count = countWordOccurrences(pageText, countInput);
+        System.out.println("The word \"" + countInput + "\" appears " + count + " times.");
+
+    }
+
+
+    // Method to count occurrences of a word in a text
+    public static int countWordOccurrences(String text, String word) {
+        Pattern pattern = Pattern.compile(word, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ src/main/java/utils/Drivers.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Drivers {
+
+    public static String getChromeDriver() {
+        String chromeDriver = "D:\\learning\\Alpha Team Learning\\QA Bootcamp\\SeleniumIntro\\chromedriver.exe";
+        return chromeDriver;
+    }
+}
