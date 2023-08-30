e7961d8e24a271fb554af402278c5ce07800dea2
Mostafa <mostfa.g.m@gmail.com>
2023-08-30 20:38:55 +0300

    added few failed tests and modified DTOs

--- src/test/java/class5/summarySeleniumExe/logic/entities/DTOs/RestaurantDTO.java
+++ src/test/java/class5/summarySeleniumExe/logic/entities/DTOs/RestaurantDTO.java
@@ -4,10 +4,11 @@ import lombok.Data;
 
 @Data
 public class RestaurantDTO {
-    private boolean success;
-    private String error;
     private String id;
     private String name;
     private double score;
     private String address;
+    private boolean success;
+    private String error;
+    private int status;
 }
--- src/test/java/class5/summarySeleniumExe/test/RestaurantTest.java
+++ src/test/java/class5/summarySeleniumExe/test/RestaurantTest.java
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
 import static class5.summarySeleniumExe.Infra.WebDriverManager.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,13 +34,13 @@ public class RestaurantTest {
 
     @BeforeAll
     public static void init() {
-        logger.info("Current method name: " +Thread.currentThread().getStackTrace()[1].getMethodName());
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
         driver = getDriver();
     }
 
     @BeforeEach
     public void setUp() {
-        logger.info("Current method name: " +Thread.currentThread().getStackTrace()[1].getMethodName());
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
 
         response = RestaurantApi.resetRestaurants();
         logger.info(response.getData());
@@ -52,13 +54,13 @@ public class RestaurantTest {
 
     @AfterAll
     public static void tearDown() {
-        logger.info("Current method name: " +Thread.currentThread().getStackTrace()[1].getMethodName());
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
         closeDriver();
     }
 
     @Test
     public void create_New_Restaurant_Validation() {
-        logger.info("Current method name: " +Thread.currentThread().getStackTrace()[1].getMethodName());
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
 
         //ACT
         restaurantListPage.createNewRestaurant(restId, restName, restAddress, restScore);
@@ -72,7 +74,7 @@ public class RestaurantTest {
 
     @Test
     public void delete_Restaurant_Validation() {
-        logger.info("Current method name: " +Thread.currentThread().getStackTrace()[1].getMethodName());
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
 
         //ARRANGE
         restId = "191";
@@ -97,14 +99,44 @@ public class RestaurantTest {
 
         //ASSERT
         assertAll(
-                () -> assertTrue(isDeleted,"Failed to delete the restaurant!")
+                () -> assertTrue(isDeleted, "Failed to delete the restaurant!")
         );
     }
 
+    @Test
+    public void delete_Restaurant_Failed() {
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+
+        //ARRANGE
+        restId = "191";
+        response = RestaurantApi.createNewRestaurant(restId, restName, restAddress, restScore);
+        logger.info(response.getData());
+
+        assertAll(
+                //status validation
+                () -> assertEquals(201, response.getStatus(), "Expected status code is 201"),
+
+                //not null validation
+                () -> assertThat(response.getData().isSuccess(), notNullValue()),
+
+                //data validation
+                () -> assertTrue(response.getData().isSuccess(), "Failed to create new restaurant!")
+        );
+
+        refreshPage();
+
+        //ASSERT
+        assertAll(
+                () -> assertThrows(TimeoutException.class, () -> {
+                    //ACT
+                    restaurantListPage.deleteRestaurant(restId + 99999);
+                })
+        );
+    }
 
     @Test
     public void update_Restaurant_Info_Validation() {
-        logger.info("Current method name: " +Thread.currentThread().getStackTrace()[1].getMethodName());
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
 
         //ARRANGE
         restId = "11211";
@@ -126,7 +158,7 @@ public class RestaurantTest {
 
         restName = "Assaf";
         restAddress = "GoTech Company";
-        response = RestaurantApi.editRestaurant(restId,restName,restAddress,restScore);
+        response = RestaurantApi.editRestaurant(restId, restName, restAddress, restScore);
         logger.info(response.getData());
 
 
@@ -149,6 +181,60 @@ public class RestaurantTest {
         //ASSERT
         assertAll(
                 () -> assertTrue(isFound, "Failed to found the restaurant!")
+        );
+    }
+
+    @Test
+    public void update_Restaurant_Info_Failed() {
+        logger.info("Current method name: " + Thread.currentThread().getStackTrace()[1].getMethodName());
+
+        //ARRANGE
+        restId = "11211";
+        response = RestaurantApi.createNewRestaurant(restId, restName, restAddress, restScore);
+        logger.info(response.getData());
+
+        assertAll(
+                //status validation
+                () -> assertEquals(201, response.getStatus(), "Expected status code is 201"),
+
+                //not null validation
+                () -> assertThat(response.getData().isSuccess(), notNullValue()),
+
+                //data validation
+                () -> assertTrue(response.getData().isSuccess(), "Failed to create new restaurant!")
+        );
+
+        refreshPage();
+
+        restName = "Assaf";
+        restAddress = "GoTech Company";
+        restId += 9999999;
+
+        response = RestaurantApi.editRestaurant(restId, restName, restAddress, restScore);
+        logger.info(response.getData());
+
+
+        assertAll(
+                //status validation
+                () -> assertEquals(404, response.getStatus(), "Expected status code is 404"),
+
+                //not null validation
+                () -> assertThat(response.getData().isSuccess(), notNullValue()),
+                () -> assertThat(response.getData().getError(), notNullValue()),
+
+                //data validation
+                () -> assertFalse(response.getData().isSuccess(), "Not Failed to update restaurant info!"),
+                () -> assertThat(response.getData().getError(), equalTo("restaurant with given id not found"))
+        );
+
+        refreshPage();
+
+        //ASSERT
+        assertAll(
+                () -> assertThrows(TimeoutException.class, () -> {
+                    //ACT
+                    restaurantListPage.findRestaurantInPage(restId, restName, restAddress, restScore);
+                })
         );
     }
 }
