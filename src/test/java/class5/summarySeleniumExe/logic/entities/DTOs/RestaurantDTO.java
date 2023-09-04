53ef7075fc46057624e041f5c999ba0f0415e5a4
Mostafa <mostfa.g.m@gmail.com>
2023-09-04 20:14:08 +0300

    modified DTO class

--- src/test/java/class5/summarySeleniumExe/logic/entities/DTOs/RestaurantDTO.java
+++ src/test/java/class5/summarySeleniumExe/logic/entities/DTOs/RestaurantDTO.java
@@ -4,10 +4,6 @@ import lombok.Data;
 
 @Data
 public class RestaurantDTO {
-    private String id;
-    private String name;
-    private double score;
-    private String address;
     private boolean success;
     private String error;
     private int status;
